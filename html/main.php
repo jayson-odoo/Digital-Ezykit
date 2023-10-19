715faadea9c76eda4d9053ca404f50437ddce48d
jayson-odoo <TehJayson@signaturegroup.com>
2023-10-19 17:31:26 +0800

    [ADD][main] bootstrap

---
+++ html/main.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'/>
+    <meta charset="utf-8" />
+    <title>Kubiq Digital Ezykit</title>
+    <meta name="description" content="app, web app, responsive, responsive layout, admin, admin panel, admin dashboard, flat, flat ui, ui kit, AngularJS, ui route, charts, widgets, components" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+    <link rel="stylesheet" href="../libs/assets/animate.css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="../libs/assets/font-awesome/css/font-awesome.css" type="text/css" />
+    <link rel="stylesheet" href="../libs/jquery/bootstrap/dist/css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="../libs/jquery/waves/dist/waves.css" type="text/css" />
+    <link rel="stylesheet" href="../libs/jquery/datatables/media/js/jquery.dataTables.css" type="text/css" />  <link rel="stylesheet" href="../libs/jquery/datepicker/datepicker.css">
+    <link rel="stylesheet" href="../libs/jquery/timepicker/bootstrap-timepicker.css">
+    <link href="../libs/jquery/fine-uploader/fine-uploader-new.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="styles/material-design-icons.css" type="text/css" />
+    <link rel="stylesheet" href="styles/font.css" type="text/css" />
+    <link rel="stylesheet" href="styles/app.css" type="text/css" />
+</head>
+<body>
+    <header class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a href="https://kubiq.com.my" class="navbar-brand">
+                <img src="../static/img/kubiq_logo.png" alt="Kubiq Logo" height="50"/>
+            </a>
+            <button class="btn btn-primary" type="button">New Design</button>
+            <form class="form-inline align-contents-end">
+                <div class="form-group" style="width: 18rem;">
+                    <label for="total_price">Total (RM)</label>
+                    <input type="text" class="form-control" id="total_price" placeholder="Total..." readonly>
+                </div>
+            </form>
+        </div>
+    </header>
+    <script src="libs/jquery/jquery/dist/jquery.js"></script>
+    <script src="libs/jquery/datatables/media/js/jquery.dataTables.js"></script>
+    <script src="libs/jquery/datatables/media/js/dataTables.bootstrap.js"></script>
+    <script src="libs/jquery/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="libs/jquery/waves/dist/waves.js"></script>
+    <script src="libs/jquery/parsley/parsley.js"></script>
+    <script src="libs/jquery/datepicker/bootstrap-datepicker.js"></script>
+    <script src="libs/jquery/timepicker/bootstrap-timepicker.js"></script>
+    <script src="libs/jquery/fine-uploader/fine-uploader.js"></script>
+    <script src="scripts/ui-load.js"></script>
+    <script src="scripts/ui-jp.config.js"></script>
+    <script src="scripts/ui-jp.js"></script>
+    <script src="scripts/ui-nav.js"></script>
+    <script src="scripts/ui-toggle.js"></script>
+    <script src="scripts/ui-waves.js"></script>
+</body>
+</html>
