75adaff8a3b77536969bffb2f76fcf805384832e
JunSheng-Signature <leejunsheng.signature@gmail.com>
2023-11-06 16:11:56 +0800

    [Fix] [staging] code for staging

--- kubiq_quotation_test.php
+++ kubiq_quotation_test.php
@@ -185,7 +185,7 @@ if(isset($_GET['ezkit']) && $_GET['ezkit'] == 'true'){
         sendData();
         
         //staging 
-        window.open(window.location.origin + "/skcrm/html/index.php?module=leads_cc_create_kubiq&"+queryString);
+        window.open(window.location.origin + "/skcrm/index.php?module=leads_cc_create_kubiq&"+queryString);
         // Live
         // window.open(window.location.origin + "/html/index.php?module=leads_cc_create_kubiq&"+queryString);
       }
